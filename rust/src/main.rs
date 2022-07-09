df2284485891d6536a87d3291b84664b680385cb
Davi Reis <reis.davi11010@gmail.com>
2022-07-09 18:14:05 -0300

    update in rust -> v0.1.2: refatored and optimized

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,97 +1,75 @@
 use std::{env::{args, Args}, io::{Stdin, stdin, Read}};
 
-fn split_lines_args(args: &mut Args) -> (Vec<String>, usize) {
-  let mut text_vector: Vec<String> = args.collect();
-  text_vector.remove(0);
-  let text: String = text_vector.join(" ");
+fn main() {
+  let lines: Vec<String> = if args().len() > 1
+    { format_args(args()) } else { format_pipe(&mut stdin()) };
 
-  // auxiliar variables
-  let mut temp_line: String = String::new();
-  let mut temp_lines: Vec<String> = Vec::new();
-  let mut temp_count: usize = 0;
-
-  // output variables
-  let mut max_size: usize = 0;
-  let mut lines: Vec<String> = Vec::new();
-
-  // split text in lines and generate the max length
-  for (i, character) in text.chars().enumerate() {
-    if ((temp_count >= 40) && (character == ' ')) || i == text.len() - 1 {
-      if character != ' ' { temp_line.push(character) }
-      temp_lines.push(temp_line.clone());
-      temp_line.clear();
-
-      max_size = if temp_count + 1 > max_size { temp_count + 1 } else { max_size };
-      temp_count = 0;
-      continue;
-    }
-
-    temp_line.push(character);
-    temp_count += 1;
-  }
-
-  // grant lines has max_size length
-  for mut line in temp_lines {
-    while line.len() < max_size {
-      line.push(' ');
-    }
-    lines.push(line);
-  }
-
-  return (lines, max_size);
+  print_cat(lines);
 }
 
-fn split_lines_pipe(pipe: &mut Stdin) -> (Vec<String>, usize) {
+// -----------------------------------------------------------------------< Util functions
+
+fn lines_max_length(lines: Vec<String>) -> usize {
+  if lines.is_empty() { 0 } else {
+    lines.into_iter().map( |line: String| line.len() ).max().expect("Lines has no max length!")
+  }
+} // If the input is empty return 0 and if doesn't return the maximum length
+
+fn fill_lines_whitespace(lines: Vec<String>) -> Vec<String> {
+  let length: usize = lines_max_length(lines.clone());
+  let lines: Vec<String> = lines.into_iter().map(
+    |mut line: String| -> String {
+      line.push_str(" ".repeat(length - line.len()).as_str());
+      return line;
+    }).collect();
+
+  lines
+} // Fill lines with whitespaces to the maximum length and return them
+
+// ----------------------------------------------------------------------< Parse functions
+
+fn format_args(args: Args) -> Vec<String> {
+  let mut text_vector: Vec<String> = args.collect();
+  text_vector.remove(0);
+
+  let mut count: usize = 0;
+  let text: String = text_vector.join(" ").chars().map(
+    |character: char| -> char {
+      if count >= 40 && character == ' ' {
+        count = 0;
+        return '\n';
+      }
+      count += 1;
+      return character;
+    }).collect();
+
+  let lines: Vec<String> = text.lines().into_iter().map( |s: &str| String::from(s) ).collect();
+  fill_lines_whitespace(lines)
+} // Format input received from args and return them
+
+fn format_pipe(pipe: &mut Stdin) -> Vec<String> {
   let mut text: String = String::new();
   let _result = pipe.read_to_string(&mut text);
 
-  let mut temp_lines: Vec<String> = Vec::new();
+  let lines: Vec<String> = text.lines().into_iter().map(
+    |line: &str| line.replace('\t', " ")
+    ).collect();
 
-  let mut lines: Vec<String> = Vec::new();
-  let mut max_size: usize = 0;
+  fill_lines_whitespace(lines)
+} // Format input received from pipe and return them
 
-  for line in text.lines() {
-    temp_lines.push(String::from(line.replace('\t', " ")));
-    max_size = if line.len() > max_size { line.len() } else { max_size };
-  }
+// -----------------------------------------------------------------------< Main functions
 
-  for mut line in temp_lines {
-    while line.len() < max_size {
-      line.push(' ');
-    }
-    lines.push(line);
-  }
-
-  return (lines, max_size);
-}
-
-fn print_cat(lines: Vec<String>, max_size: usize) {
-  println!("          _-{}-_", "-".repeat(max_size));
+fn print_cat(lines: Vec<String>) {
+  let length: usize = lines[0].len();
+  println!("          _-{}-_", "-".repeat(length));
   for line in lines {
     print!("         |  {}  |\n", line);
   }
-  println!("        / ¯-{}-¯", "-".repeat(max_size));
-  println!("       /     ");
-  println!(" ／l、        ");
-  println!("（ﾟ､ ｡ ７     ");
-  println!("|、ﾞ ~ヽ      ");
-  println!("じし f\\_, )ノ");
-}
-
-fn main() {
-  // Define util variable
-  let mut lines: (Vec<String>, usize);
-
-  // Receive command text when args
-  let mut input_args: Args = args();
-  lines = split_lines_args(&mut input_args);
-
-  // Receive command form pipe and use if there's no other input
-  let mut input_pipe: Stdin = stdin();
-  if lines.0.is_empty() {
-    lines = split_lines_pipe(&mut input_pipe);
-  }
-
-  // Print output
-  print_cat(lines.0, lines.1);
-}
+  println!("        / ¯-{}-¯", "-".repeat(length));
+  println!(r#"       /
+   ／l、
+  （ﾟ､ ｡ ７
+  |、ﾞ ~ヽ
+  じし f\_, )ノ"#);
+} // Print text formatted and the cat
